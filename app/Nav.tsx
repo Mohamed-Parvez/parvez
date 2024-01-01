acdb7d676f3660ee23b06a5aac02bec56fee4ab4
Mohamed-Parvez <clash.parwesh@gmail.com>
2024-01-01 23:04:10 +0530

    Add list of Nav Items

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -1,11 +1,22 @@
 import ImageComponent from "./components/ImageComponent";
 import ButtonComponent from "./components/ButtonComponent";
+import { ListOfNav } from "@/constants/utilities";
+import Link from "next/link";
 
 const Nav = () => {
   return (
     <nav className="flex-between m-3 max-w-[1440px] 2xl:m-3 2xl:mx-auto">
       <ImageComponent src="/" alt="logo" height={30} width={30} />
-      <ButtonComponent label="Let's Talk" classname="btn" />
+      <div className="md:flex-between hidden gap-7 text-[16px] md:block">
+        <ul className="flex-between gap-7 font-sans">
+          {ListOfNav.map((data) => (
+            <Link key={data.key} href={data.href}>
+              <li>{data.label}</li>
+            </Link>
+          ))}
+        </ul>
+        <ButtonComponent label="Let's Talk" classname="btn" />
+      </div>
     </nav>
   );
 };
---
+++ constants/utilities.tsx
@@ -0,0 +1,6 @@
+export const ListOfNav = [
+  { key: "about", label: "About", href: "/about" },
+  { key: "products", label: "Products", href: "/products" },
+  { key: "services", label: "Services", href: "/services" },
+  { key: "projects", label: "Projects", href: "/projects" },
+];
