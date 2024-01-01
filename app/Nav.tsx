108c6c1038e05da29241d4aa365f9a3b61bbcdad
Mohamed-Parvez <clash.parwesh@gmail.com>
2024-01-01 23:09:40 +0530

    Add Nav corrections

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -7,15 +7,19 @@ const Nav = () => {
   return (
     <nav className="flex-between m-3 max-w-[1440px] 2xl:m-3 2xl:mx-auto">
       <ImageComponent src="/" alt="logo" height={30} width={30} />
-      <div className="md:flex-between hidden gap-7 text-[16px] md:block">
-        <ul className="flex-between gap-7 font-sans">
-          {ListOfNav.map((data) => (
-            <Link key={data.key} href={data.href}>
-              <li>{data.label}</li>
-            </Link>
-          ))}
-        </ul>
-        <ButtonComponent label="Let's Talk" classname="btn" />
+      <div className="md:flex-between gap-7 text-[16px]">
+        <div className="hidden md:block">
+          <ul className="flex-between gap-7 font-sans">
+            {ListOfNav.map((data) => (
+              <Link key={data.key} href={data.href}>
+                <li>{data.label}</li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <Link href={""}>
+          <ButtonComponent label="Let's Talk" classname="btn" />
+        </Link>
       </div>
     </nav>
   );
