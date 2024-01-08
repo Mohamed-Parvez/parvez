9ffe6995a7b6432aa28a88ebadc2f7bacd10f280
Mohamed-Parvez <clash.parwesh@gmail.com>
2024-01-08 19:50:14 +0530

    Add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Contact from "@/components/forms/Contact";
 import About from "@/components/sections/About";
+import Hero from "@/components/sections/Hero";
 import Products from "@/components/sections/Products";
 import Projects from "@/components/sections/Projects";
 import Services from "@/components/sections/Services";
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <>
       <main>
+        <Hero />
         <About />
         <Products />
         <Services />
