2697188a4a0eee2f4a2231dccbca755c379860a8
Mohamed-Parvez <clash.parwesh@gmail.com>
2024-01-11 23:23:06 +0530

    Final hero UI design

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -4,7 +4,7 @@ import Button from "../ui/button";
 
 const Hero = () => {
   return (
-    <div id="hero" className="flex-center mt-10 flex-col pb-96">
+    <div id="hero" className="flex-center mt-16 flex-col pb-96">
       <div
         // eslint-disable-next-line tailwindcss/migration-from-tailwind-2
         className="flex-center mx-3 h-auto w-auto cursor-pointer gap-1.5 text-wrap
@@ -22,20 +22,21 @@ const Hero = () => {
         />
       </div>
 
-      <div className="mt-4 flex-col items-center justify-center">
+      <div className="mt-4 flex-col items-center justify-center lg:mt-8">
         <h1
           className="gap-2 text-center font-Sans text-4xl font-extrabold 
-        leading-[1.4] md:text-5xl md:leading-[1.4] lg:text-6xl lg:leading-[1.4]"
+        leading-[1.4] md:text-5xl md:leading-[1.4] lg:text-6xl lg:leading-[1.4] 
+        xl:text-7xl xl:leading-[90px]"
         >
           Hey, Hi There !
           <br />
           <span className="primary-gradient">I am Parvez</span>
         </h1>
 
-        <div className="m-3 mt-4 h-auto w-auto max-w-[380px] md:max-w-[500px]">
+        <div className="m-3 mt-4 h-auto w-auto max-w-[380px] md:max-w-[500px] lg:mt-8">
           <h2
-            className="mt-4 text-center font-Sans text-[15px] font-medium
-           text-Grey_700 md:text-lg lg:text-xl"
+            className="mt-4 text-center font-Sans text-[15px] font-medium text-Grey_700
+           md:text-lg lg:mt-8 lg:text-xl"
           >
             Web Developer for over past 2 Years and made Unique Websites using
             Advanced Web Tools
@@ -43,7 +44,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="flex-center mt-4 h-auto w-auto gap-3">
+      <div className="flex-center mt-4 h-auto w-auto gap-3 lg:mt-8">
         <Button label="Resume" variant={"btn-resume"} type={"button"} href="" />
         <Button label="Github" variant={"btn-github"} type={"button"} href="" />
       </div>
