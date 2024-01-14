31382d603bccc6451b62d1d4ac7ea10e6f1b99e2
Mohamed-Parvez <clash.parwesh@gmail.com>
2024-01-14 17:16:21 +0530

    updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,24 +7,62 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xs: "400px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1220px",
+      "2xl": "1440px",
+      "3xl": "1700px",
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
         Sans: ["sans"],
+        poppins: ["Poppins", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
       },
       colors: {
-        Black: "#000000",
-        Light_Black: "#222831",
-        Grey: "#EEEEEE",
-        White: "#FFFFFF",
-        Amber_500: "#FFC107",
-        Yellow_500: "#FFEB3B",
-        Orange_600: "#FB8C00",
-        Grey_700: "#616161",
+        primary: {
+          purple: "#8E24AA",
+          pink: "#D81B60",
+        },
+        grey: {
+          DEFAULT: "#EEEEEE",
+          100: "#969BA5",
+          200: "#55616D",
+          700: "#616161",
+        },
+        black: {
+          DEFAULT: "#000",
+          100: "#0D1117",
+          200: "#161B22",
+          300: "#1F2428",
+          400: "#242C38",
+        },
+        white: {
+          DEFAULT: "#FFF",
+          400: "#A3B3BC",
+          500: "#A4B8D5",
+          800: "#D0DFFF",
+        },
+        amber: {
+          500: "#FFC107",
+        },
+        yellow: {
+          500: "#FFEB3B",
+        },
+        orange: {
+          600: "#FB8C00",
+        },
         transparent: "transparent",
-        Purple_600: "#8E24AA",
-        Pink_600: "#D81B60",
-        Blue: "#4ca5ff",
-        Violet: "#b573f8",
       },
     },
   },
