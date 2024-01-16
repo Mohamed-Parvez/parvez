3e3da8cb1d98dbf901dd9795f4026e0c098a5ce9
Mohamed-Parvez <clash.parwesh@gmail.com>
2024-01-16 10:54:18 +0530

    updated navbar

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -4,19 +4,21 @@ import Navlist from "../ui/navlist";
 
 const Nav = () => {
   return (
-    <nav
-      className="flex-between fixed top-0 z-50 w-full 
+    <nav className="flex-center">
+      <div
+        className="flex-between fixed top-0 z-50 w-full 
       max-w-screen-2xl bg-white p-3 2xl:mx-auto"
-    >
-      <Logo />
-      <div className="md:flex-between gap-7">
-        <Navlist />
-        <Button
-          label="Let's Talk"
-          variant={"btn"}
-          type={"button"}
-          href="#contact"
-        />
+      >
+        <Logo />
+        <div className="md:flex-between gap-7">
+          <Navlist />
+          <Button
+            label="Let's Talk"
+            variant={"btn"}
+            type={"button"}
+            href="#contact"
+          />
+        </div>
       </div>
     </nav>
   );
