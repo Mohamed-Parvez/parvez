e237a1d505cbca3a401b21a5b22dfcb808323293
Mohamed-Parvez <clash.parwesh@gmail.com>
2024-01-17 20:39:11 +0530

    Add smooth scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 import Nav from "../components/shared/Navbar";
 import React from "react";
 import Footer from "@/components/shared/Footer";
+import SmoothScrolling from "@/components/ui/smoothscrolling";
 
 export const metadata: Metadata = {
   title: "Parvez",
@@ -19,7 +20,7 @@ export default function RootLayout({
       <body>
         <Nav />
         <main className="mx-5 mt-[110px] min-h-screen font-poppins md:mx-10 xl:mx-28">
-          {children}
+          <SmoothScrolling>{children}</SmoothScrolling>
         </main>
         <Footer />
       </body>
---
+++ components/ui/smoothscrolling.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import { ReactLenis } from "@studio-freight/react-lenis";
+
+function SmoothScrolling({ children } : any) {
+  return (
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothTouch: true }}>
+      {children}
+    </ReactLenis>
+  );
+}
+
+export default SmoothScrolling;
