1991fc8f19a5de2b1d8763f5d5a4dce0c04b66bb
Mohamed-Parvez <clash.parwesh@gmail.com>
2024-01-17 19:20:48 +0530

    Add Footer section

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="flex-center">
+      <div
+        className="flex-between w-full max-w-[1200px]
+       gap-12 bg-white p-3 md:p-6 2xl:mx-auto"
+      >
+        <div className="flex-between gap-1">
+          <Image
+            src={"/assets/images/copyright.png"}
+            alt="copyright"
+            height={16}
+            width={16}
+          />
+          <p className="small-bold mt-[2px] md:text-[20px] md:font-semibold">2024</p>
+        </div>
+        <div>
+          <p className="small-regular md:body-regular text-gray-700">
+            Designed and Developed by Mohamed Parvez
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
