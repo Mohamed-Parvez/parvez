f909e0d332ecfe96f4f49f68aaba4cef8de928ba
Mohamed-Parvez <clash.parwesh@gmail.com>
2024-01-23 14:32:00 +0530

    Add Final contact API

--- components/forms/EmailMessage.tsx
+++ components/forms/EmailMessage.tsx
@@ -26,6 +26,7 @@ const MessageUsEmail = ({ name, email, message }: MessageUsEmailProps) => {
           <Container className="mx-auto my-[20px] max-w-4xl p-[20px]">
             <Heading className="text-left text-[20px] font-normal text-black">
               <strong>This is {name},</strong>
+              <Text className="text-[12px] text-[#666666]">Id: {email}</Text>
             </Heading>
             <Hr className="mx-0 my-[16px] w-full" />
             <Text className="text-[12px] text-[#666666]">{message}</Text>
