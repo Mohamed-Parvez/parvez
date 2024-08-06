c1f7c5593ed4ed84b01e576405c865c18b614fb7
Mohamed-Parvez <clash.parwesh@gmail.com>
2024-08-06 09:46:20 +0530

    updated resume

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -50,7 +50,7 @@ const Hero = () => {
           label="Resume"
           variant={"btn-resume"}
           type={"button"}
-          href="https://res.cloudinary.com/dlwlrv6c4/image/upload/f_auto,q_auto/ukkgwclaje5tbhuccaax"
+          href="https://res.cloudinary.com/dlwlrv6c4/image/upload/f_auto,q_auto/x3wont0qx6lhoo1o93co"
         />
         <ButtonPopup
           label="Github"
